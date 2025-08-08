e26d0ee75dfeaa57724d3de9bc1c9476a9841d55
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-08-09 08:19:31 +1000

    Improve home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,18 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center relative">
+    <main className="min-h-svh flex flex-col items-center justify-center relative">
       <HalfToneGradient className="absolute inset-0" />
       
       <div className="relative z-10 text-center flex gap-8 flex-col items-center px-4">
-        <h1 className="text-5xl max-w-2xl">
+        <h1 className="text-3xl sm:text-5xl max-w-lg sm:max-w-2xl">
           Effortless attendance across meetings and teams.
         </h1>
-        <p className="text-xl text-muted-foreground max-w-lg">
+        <p className="text-lg sm:text-xl text-muted-foreground max-w-80 sm:max-w-lg">
           Import users and set up teams in seconds. Collect attendance securely with a single, verified link.
         </p>
         <div className="flex gap-4 justify-center">
-          <Button asChild className="p-8 py-5 rounded-full" variant={"ghost"}>
-            <Link href="">See how it works</Link>
-          </Button>
-          <Button asChild className="p-8 py-5 rounded-full">
+          <Button asChild className="p-8 py-6 rounded-full font-semibold text-md">
             <Link href="/auth/login">Get started for free</Link>
           </Button>
         </div>
